12d1bf67e8592587f7660d11d8e9a133bc74ea3f
Dmitrii Onegin <dimimimo@mail.ru>
2022-02-20 16:31:51 +0300

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Dmitrii Onegin on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
